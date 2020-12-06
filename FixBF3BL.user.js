33181404fa5b7fc502df6edb6d92474183e7f43c
Maximilian Kenfenheuer <maximilian@mkenfenheuer.de>
2020-12-06 16:37:29 +0100

    Update FixBF3BL.user.js

--- FixBF3BL.user.js
+++ FixBF3BL.user.js
@@ -11,8 +11,8 @@
 // The below code will replace all relevant hosted JS assets with custom ones.
 
 //438223020
-const old_data = "//eaassets-a.akamaihd.net/bl-cdn/cdnprefix/production-284-20170531/public/generated/en_US/bundle_base_bottombundles_438223020.js";
-const new_data = "https://rawcdn.githack.com/mKenfenheuer/BF3-Limited-Edition-B2K-Fix/80b2cd40f6dfd512bf55ad07f5b28dd169f38a77/bundle_base_bottombundles_438223020.js?raw=true";
+const old_data = "//eaassets-a.akamaihd.net/bl-cdn/cdnprefix/production-5766-20200917/public/generated/en_US/bundle_base_bottombundles_438223020.js";
+const new_data = "https://rawcdn.githack.com/mKenfenheuer/BF3-Limited-Edition-B2K-Fix/cfd0d81fdd0c70a24a584030727e953a056e016a/bundle_base_bottombundles_438223020.js?raw=true";
 
 function injectScript(originalPage) { //Function injectScript replaces the file
     console.log('Performing inection, please wait...');
